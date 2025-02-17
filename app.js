27eb3cb2e365accc3f011cfcb039331e44b21127
rafaklugee <rafaelkluge2010@gmail.com>
2025-02-17 18:14:15 -0300

    ainda tem muita coisa...

---
+++ app.js
@@ -0,0 +1,60 @@
+// Carregando módulos
+    const express = require('express')
+    const handlebars = require('express-handlebars')
+    const bodyParser = require('body-parser')
+    const app = express()
+    const path = require('path')
+    const session = require('express-session')
+    const flash = require('connect-flash')
+    const passport = require('passport')
+
+// Configurações
+    // Sessão
+    app.use(session({
+        secret: "fikc",
+        resave: true,
+        saveUninitialized: true
+    }))
+    app.use(passport.initialize())
+    app.use(passport.session())
+    app.use(flash())
+    // Middleware
+        app.use((req, res, next) => {
+            res.locals.success_msg = req.flash("success_msg")
+            res.locals.error_msg = req.flash("error_msg")
+            res.locals.error = req.flash("error")
+            res.locals.user = req.user || null
+            next()
+        });
+    // Body Parser
+        app.use(bodyParser.urlencoded({extended: true}))
+        app.use(bodyParser.json())
+    // HandleBars
+        app.engine('handlebars', handlebars.engine({defaultLayout: 'main'}))
+        app.set('view engine', 'handlebars')
+    // Public
+    app.use(express.static(path.join(__dirname,'public')))
+
+// Rotas
+    
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.get('/login', (req, res) => {
+    res.render('login');
+});
+
+app.get('/politica-de-privacidade', (req, res) => {
+    res.render('politica-de-privacidade');
+});
+
+app.get('/termos-de-uso', (req, res) => {
+    res.render('termos-de-uso');
+});
+
+// Outros
+const PORT = 5432;
+app.listen(PORT, () => {
+    console.log("Servidor rodando! ");
+})
