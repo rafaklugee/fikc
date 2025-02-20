12c7349a61bf00915e415af17bb717c9c6e5e020
rafaklugee <rafaelkluge2010@gmail.com>
2025-02-20 19:35:44 -0300

    Fix PORT

--- app.js
+++ app.js
@@ -80,7 +80,7 @@ app.use('/usuarios', usuarios);
 app.use('/postagem', postagemRoutes);
 
 // Outros
-const PORT = 8081;
+const PORT = process.env.PORT || 8081;
 app.listen(PORT, () => {
     console.log("Servidor rodando!");
 })
